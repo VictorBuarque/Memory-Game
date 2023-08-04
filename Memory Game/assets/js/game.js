2bbfbfa387612ca8aba21af4d0e2681398a6a293
Victor Gabriel Reis Buarque da Silva <114040791+VictorBuarque@users.noreply.github.com>
2023-08-04 03:57:15 -0300

    Third Commit
    Created new tag button in game.html.
    Created css for this button and other classes.
    Created many functions in JS.

--- Memory Game/assets/js/game.js
+++ Memory Game/assets/js/game.js
@@ -1,102 +1,127 @@
+// DOM Elements
 const grid = document.querySelector('.grid');
+const timer = document.querySelector('.timer');
+const spanPlayer = document.querySelector('.player');
+const button = document.querySelector('button');
 
+// Card Numbers
 const actors = [
-    '1',
-    '2',
-    '3',
-    '4',
-    '5',
-    '6',
-    '7',
-    '8',
-    '9',
-    '10'
+    '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'
 ];
 
+// Game variables
 let firstCard = '';
 let secondCard = '';
+let loop;
 
-
-// Reveal function
-const revealCard = ({target}) =>{
-    if (target.parentNode.className.includes('back')) { //condition for card flip
+// Revealing cards
+const revealCard = ({ target }) => {
+    if (target.parentNode.classList.contains('back')) {
         return;
     }
-    if(firstCard ===''){ //Verifying if the first card flipped
-        target.parentNode.classList.add('reveal-card'); //select class and add to card
+    if (firstCard === '') {
+        target.parentNode.classList.add('reveal-card');
         firstCard = target.parentNode;
-    } else if (secondCard === ''){ //Verifying if is the second card flipped
+    } else if (secondCard === '') {
         target.parentNode.classList.add('reveal-card');
         secondCard = target.parentNode;
-        
         checkCard();
     }
-    
 }
-const checkWinner = ()=>{
-    const disabled = document.querySelectorAll('.disabled-card'); 
+
+// Verify Winner
+const checkWinner = () => {
+    const disabled = document.querySelectorAll('.disabled-card');
     if (disabled.length === 20) {
-        alert('Winner');
+        button.removeAttribute('disabled');
+        clearInterval(loop);
+        alert(`Parabéns ${spanPlayer.innerHTML}, seu tempo foi de ${timer.innerHTML}`);
     }
 }
-//Check card function
+
+// Verify card functionality
 const checkCard = () => {
     const firstActor = firstCard.getAttribute('data-actors');
     const secondActor = secondCard.getAttribute('data-actors');
 
     if (firstActor === secondActor) {
-
         firstCard.firstChild.classList.add('disabled-card');
         secondCard.firstChild.classList.add('disabled-card');
         firstCard = '';
         secondCard = '';
-
-        checkWinner()
-
+        checkWinner();
     } else {
-        setTimeout(() => { //This function will flip back if you pick wrong cards
-        firstCard.classList.remove('reveal-card');
-        secondCard.classList.remove('reveal-card');
-        firstCard = '';
-        secondCard = '';
-        },500);
+        setTimeout(() => {
+            firstCard.classList.remove('reveal-card');
+            secondCard.classList.remove('reveal-card');
+            firstCard = '';
+            secondCard = '';
+        }, 500);
     }
 }
-//Creating HMTL tag and class 
+
+// Creating HTML elements 
 const createElement = (tag, className) => {
-    const element = document.createElement(tag); //creating a html tag using createElement function
-    element.className = className; //add class to tag
-    return element; 
+    const element = document.createElement(tag);
+    element.className = className;
+    return element;
 }
 
-//Creating card
-const createCard = (actors)=>{
-
+// Creating cards
+const createCard = (actors) => {
     const card = createElement('div', 'card');
     const front = createElement('div', 'face front');
     const back = createElement('div', 'face back');
 
-    
     front.style.backgroundImage = `url('./assets/img/${actors}.jpeg')`;
 
     card.appendChild(front);
     card.appendChild(back);
     card.addEventListener('click', revealCard);
     card.setAttribute('data-actors', actors);
-    
+
     return card;
-    
 }
-//Loading function
+
+// Load game function
 const loadGame = () => {
     const duplicateActors = [...actors, ...actors];
-    const shuffleCards = duplicateActors.sort(() => Math.random() - 0.5); // Shuffle the cards
+    const shuffleCards = duplicateActors.sort(() => Math.random() - 0.5);
 
-    //Creating each cards
-    shuffleCards.forEach((actor) => { // Use 'actor' instead of 'card' here
-        const card = createCard(actor); // Pass 'actor' instead of 'actors'
+    shuffleCards.forEach((actor) => {
+        const card = createCard(actor);
         grid.appendChild(card);
     });
 }
 
-loadGame();
+// Start timer function
+const startTimer = () => {
+    loop = setInterval(() => {
+        const currentTimer = Number(timer.innerHTML);
+        timer.innerHTML = currentTimer + 1;
+    }, 1000);
+}
+
+// Start the game when window is fully loaded
+window.onload = () => {
+    spanPlayer.innerHTML = localStorage.getItem('player');
+    startTimer();
+    loadGame();
+
+    // Restart event
+    button.addEventListener('click', restart);
+};
+
+// Restart game
+function restart() {
+    clearPage();
+    clearInterval(loop);
+    timer.innerHTML = '0';
+    startTimer();
+    loadGame();
+}
+
+// Clear Page
+function clearPage() {
+    grid.innerHTML = '';
+}
